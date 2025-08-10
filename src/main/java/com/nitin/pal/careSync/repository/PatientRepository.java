bea1999e6e21e9cff26512b39782c700782046d5
nitinkumarpals <nitinpal900@gmail.com>
2025-08-10 19:40:48 +0530

    Added query method in jpa, Query anotion for custom jpql and group by query in jpl

--- src/main/java/com/nitin/pal/careSync/repository/PatientRepository.java
+++ src/main/java/com/nitin/pal/careSync/repository/PatientRepository.java
@@ -1,10 +1,30 @@
 package com.nitin.pal.careSync.repository;
 
 import com.nitin.pal.careSync.entity.Patient;
+import com.nitin.pal.careSync.entity.type.BloodGroupType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Repository
 public interface PatientRepository extends JpaRepository<Patient,Long> {
     Patient findByName(String name);
+    List<Patient> findByBirthDateOrEmail(LocalDate birthDate, String email);
+
+    List<Patient> findByBirthDateBetween(LocalDate startDate, LocalDate endDate);
+
+    List<Patient> findByNameContainingOrderByIdDesc(String name);
+
+    @Query("SELECT p FROM Patient p where p.bloodGroup = :bloodGroup")
+    List<Patient> findByBloodGroup(@Param("bloodGroup") BloodGroupType bloodGroup);
+
+    @Query("SELECT p FROM Patient p where p.birthDate > :birthDate")
+    List<Patient> findByBornAfterDate(@Param("birthDate") LocalDate birthDate);
+
+    @Query("SELECT p.bloodGroup, Count(p) FROM Patient p group by p.bloodGroup")
+    List<Object[]> countEachBloodGroupByType();
 }
--- src/test/java/com/nitin/pal/careSync/PatientTests.java
+++ src/test/java/com/nitin/pal/careSync/PatientTests.java
@@ -1,12 +1,15 @@
 package com.nitin.pal.careSync;
 
 import com.nitin.pal.careSync.entity.Patient;
+import com.nitin.pal.careSync.entity.type.BloodGroupType;
 import com.nitin.pal.careSync.repository.PatientRepository;
 import com.nitin.pal.careSync.service.PatientService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.List;
 
 @SpringBootTest
@@ -26,7 +29,13 @@ public class PatientTests {
     @Test
     public void testTransactionMethods(){
 //        Patient patient = patientService.getPatientById(27L);
-        Patient patient= patientRepository.findByName("Diya Patel");
-        System.out.println(patient);
+//        Patient patient= patientRepository.findByName("Diya Patel");
+        List<Patient> patientList = patientRepository.findByBirthDateOrEmail(LocalDate.of(1988,3,15),"kabir.singh@example.com" );
+        System.out.println(patientList);
+//        List<Patient> patientList2 = patientRepository.findByBornAfterDate(LocalDate.of(1993,3,14));
+        List<Object[]> bloodGroupByTypeList = patientRepository.countEachBloodGroupByType();
+        for (Object[] patient: bloodGroupByTypeList){
+            System.out.println(patient[0]+" "+ patient[1]);
+        }
     }
 }

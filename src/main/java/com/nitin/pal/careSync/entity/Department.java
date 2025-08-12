5f3737171e9751b53bf402d7a4cbf21f4ae03007
Nitin Kumar Pal <147614997+nitinkumarpals@users.noreply.github.com>
2025-08-12 19:39:09 +0530

    Update Department.java

--- src/main/java/com/nitin/pal/careSync/entity/Department.java
+++ src/main/java/com/nitin/pal/careSync/entity/Department.java
@@ -3,17 +3,31 @@ package com.nitin.pal.careSync.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class Deparment {
+public class Department {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false, unique = true, length = 100)
     private String name;
+
+    @OneToOne
+    private Doctor headDoctor;
+
+    @ManyToMany
+    @JoinTable(
+            name = "department_doctors",
+            joinColumns = @JoinColumn(name = "department_id"),
+            inverseJoinColumns = @JoinColumn(name = "doctors_id")
+    )
+    private Set<Doctor> doctors = new HashSet<>();
 }

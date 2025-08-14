f7ac754d360969e131fffc7b0e3642448f185860
Nitin Kumar Pal <147614997+nitinkumarpals@users.noreply.github.com>
2025-08-14 17:16:56 +0530

    Update InsuranceTest.java

--- src/test/java/com/nitin/pal/careSync/InsuranceTest.java
+++ src/test/java/com/nitin/pal/careSync/InsuranceTest.java
@@ -1,19 +1,25 @@
 package com.nitin.pal.careSync;
 
+import com.nitin.pal.careSync.entity.Appointment;
 import com.nitin.pal.careSync.entity.Insurance;
 import com.nitin.pal.careSync.entity.Patient;
+import com.nitin.pal.careSync.service.AppointmentService;
 import com.nitin.pal.careSync.service.InsuranceService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @SpringBootTest
 public class InsuranceTest {
 
     @Autowired
     private InsuranceService insuranceService;
+
+    @Autowired
+    private AppointmentService appointmentService;
 
     @Test
     public void testInsurance() {
@@ -25,5 +31,23 @@ public class InsuranceTest {
 
         Patient patient = insuranceService.assignInsuranceToPatient(insurance,1L);
         System.out.println(patient);
+
+        Patient disaccociatedPatient = insuranceService.disaccociateInsuranceFromPatient(patient.getId());
+        System.out.println(disaccociatedPatient);
+    }
+
+    @Test
+    public void testAppointment(){
+        Appointment appointment = Appointment
+                .builder()
+                .appointmentTime(LocalDateTime.of(2025, 11, 1, 14, 0))
+                .reason("Headache")
+                .build();
+
+        Appointment newAppointment = appointmentService.createNewAppointment(appointment, 1L, 2L);
+        System.out.println(newAppointment);
+
+        Appointment updatedAppointment = appointmentService.reAssignAppointmentToAnotherDoctor(newAppointment.getId(), 3L);
+        System.out.println(updatedAppointment);
     }
 }
